03ea3e7d355b1ae01afdf90fad88224597278143
Bhuvan Bhardwaj <ffbhuvan62@gmail.com>
2026-07-14 18:18:10 +0530

    Day 2: Learned Python Variables

---
+++ Day02_Variables.py
@@ -0,0 +1,12 @@
+name = "Bhuvan"
+
+age = 19
+
+height = 5.11
+
+is_ai_student = True
+
+print("Name : ", name)
+print("Age : ", age)
+print("Height : ", height)
+print("Learning AI : ", is_ai_student)

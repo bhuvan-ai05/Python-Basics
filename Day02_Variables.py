04defb39765b4806fdd243e4ada57bde97fa64cf
Bhuvan Bhardwaj <ffbhuvan62@gmail.com>
2026-07-14 18:29:10 +0530

    Day 2: Learned Python Variables

--- Day02_Variables.py
+++ Day02_Variables.py
@@ -10,3 +10,13 @@ print("Name : ", name)
 print("Age : ", age)
 print("Height : ", height)
 print("Learning AI : ", is_ai_student)
+
+
+print("\nMy Details")
+
+
+city = "Bijnor"
+college = "DIHSA INSTITUTE OF SCIENCE AND TECHNOLOGY"
+
+print("City : ", city)
+print("College : ", college)
